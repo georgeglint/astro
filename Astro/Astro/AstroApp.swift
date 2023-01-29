040f55114c3fc83ba86eddecefbef479f13c0fb2
George Leadbeater <georgeleadbeaterdev@gmail.com>
2023-01-29 12:36:48 +0000

    Creates the Xcode project using a SwiftUI interface and tests target

---
+++ Astro/Astro/AstroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroApp.swift
+//  Astro
+//
+//  Created by George Leadbeater on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AstroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
